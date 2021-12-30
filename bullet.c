de43edb2aed8f9d1b867fa32e3b9e6dbe99294de
BattleIsOver <mulas8923@gmail.com>
2021-12-30 17:49:20 -0500

    Bullets now follow a predictable trajectory

--- bullet.c
+++ bullet.c
@@ -1,21 +1,41 @@
 #include "bullet.h"
+#include <math.h>
 
 void bullet(int pipeOUT, borders borders, vettore direzione, coordinate startingPoint){
   bool stop = false;
+  int i=0;
 
   /* Inizializzazione coordinate */
   coordinate coords = startingPoint;
   coords.PID = getpid();
-  float mov_x = coords.x;
-  float mov_y = coords.y;
+  double mov_x = coords.x;
+  double mov_y = coords.y;
   /* prev_coordinate già inizializzate dalla funzione chiamante */
-
+  int numMovimenti = abs(direzione.x) + abs(direzione.y); /* Numero di passi da eseguire */
+  int changeAfter;
+  if(abs(direzione.x) > abs(direzione.y)){
+    changeAfter = abs(direzione.x/direzione.y);
+  }else{
+    changeAfter = abs(direzione.y/direzione.x);
+  }
   while(!stop){
     /* Aggiorno coordinate */
-    mov_x += (direzione.x * direzione.speed);
-    mov_y += (direzione.y * direzione.speed);
-    coords.x = mov_x; 
-    coords.y = mov_y;
+    if(i == changeAfter){
+      if(abs(direzione.x) > abs(direzione.y)){
+        if(direzione.y < 0) coords.y--; else coords.y++;
+      }else{
+        if(direzione.x < 0) coords.x--; else coords.x++;
+      }
+      i=0;
+    }else{
+      i++;
+      if(abs(direzione.x) > abs(direzione.y)){
+        if(direzione.x < 0) coords.x--; else coords.x++;
+      }else{
+        if(direzione.y < 0) coords.y--; else coords.y++;
+      }
+    }
+    
     /* Controllo se è fuori dallo schermo */
     if(coords.x < 1 || coords.x >= borders.maxx || coords.y < 1 || coords.y >= borders.maxy){
       coords.x = -2;
@@ -27,6 +47,6 @@ void bullet(int pipeOUT, borders borders, vettore direzione, coordinate starting
     coords.prev_coordinate.x = coords.x;
     coords.prev_coordinate.y = coords.y;
 
-    napms(DELAY_MS*DEFAULT_SPEED);
+    napms(DELAY_MS*(DEFAULT_SPEED*numMovimenti));
   }
 }
