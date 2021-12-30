55f472a446788d06eb0ff73f91ebd8e5bfee4d51
BattleIsOver <mulas8923@gmail.com>
2021-12-30 18:37:37 -0500

    No reason to keep this at 4, move it to unary value

--- utils_struct.c
+++ utils_struct.c
@@ -1,4 +1,4 @@
 #include "utils_struct.h"
 
-vettore RIGHT_UP = {4,-1, DEFAULT_SPEED};
-vettore RIGHT_DOWN = {4,1, DEFAULT_SPEED};
+vettore RIGHT_UP = {1,-1, DEFAULT_SPEED};
+vettore RIGHT_DOWN = {1,1, DEFAULT_SPEED};
